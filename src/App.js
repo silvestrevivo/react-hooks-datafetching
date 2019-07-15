829206afa8495e275e4cc6125ded1c4f63411177
silvestrevivo <silvestrevivomillan@gmail.com>
2019-07-15 10:05:46 +0200

    simple fetching data with useEffect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://hn.algolia.com/api/v1/search?query=reacthooks')
+      .then(response => {
+        // console.log('response.data.hits =>', response.data.hits);
+        setResults(response.data.hits)
+      });
+  }, []);
+
   return (
-    <div className="App">
-      Data fetching app with react hooks
-    </div>
+    <>
+      <ul>
+        {results.map(result => (
+          <li key={result.objectID}>
+            <a href={result.url}>{result.title}</a>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
