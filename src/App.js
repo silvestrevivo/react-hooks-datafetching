672b55f57c5d39c6c83c7332d4c92769c383b20d
silvestrevivo <silvestrevivomillan@gmail.com>
2019-12-06 10:12:26 +0100

    submitting form and useRef hook

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 
 function App() {
   const [results, setResults] = useState([]);
-  const [query, setQuery] = useState('reactHooks')
+  const [query, setQuery] = useState('react hooks');
+  const searchInputRef = useRef();
 
   useEffect(() => {
     getResults();
     // eslint-disable-next-line
-  }, [query]); //this hook is working with mounting component and with query changes
+  }, []);
 
   const getResults = async () => {
     const response = await axios.get(`https://hn.algolia.com/api/v1/search?query=${query}`);
     setResults(response.data.hits);
-    // This can not be inside the useEffect hooks.
+    // This can not be inside the useEffect hooks, otherwise it creates an infinite loop
+  }
+
+  const handleSearch = e => {
+    e.preventDefault();
+    getResults();
+  }
+
+  const handleClearSearch = () => {
+    setQuery('');
+    searchInputRef.current.focus();
   }
 
   return (
     <>
-      <input type="text" onChange={event => setQuery(event.target.value)} />
+      <form onSubmit={handleSearch}>
+        <input
+          type="text"
+          value={query}
+          ref={searchInputRef}
+          onChange={event => setQuery(event.target.value)} />
+        <button type="submit">Search</button>
+        <button type="button" onClick={handleClearSearch}>Clear</button>
+      </form>
       <ul>
         {results.map(result => (
           <li key={result.objectID}>
