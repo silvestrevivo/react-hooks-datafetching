f8632c308734c2194bc5348556c3f4cff335867e
silvestrevivo <silvestrevivomillan@gmail.com>
2019-12-06 09:23:41 +0100

    updating useEffect to query data

--- src/App.js
+++ src/App.js
@@ -3,19 +3,22 @@ import axios from 'axios';
 
 function App() {
   const [results, setResults] = useState([]);
+  const [query, setQuery] = useState('reactHooks')
 
   useEffect(() => {
     getResults();
-  }, []);
+    // eslint-disable-next-line
+  }, [query]); //this hook is working with mounting component and with query changes
 
   const getResults = async () => {
-    const response = await axios.get('https://hn.algolia.com/api/v1/search?query=reacthooks');
+    const response = await axios.get(`https://hn.algolia.com/api/v1/search?query=${query}`);
     setResults(response.data.hits);
     // This can not be inside the useEffect hooks.
   }
 
   return (
     <>
+      <input type="text" onChange={event => setQuery(event.target.value)} />
       <ul>
         {results.map(result => (
           <li key={result.objectID}>
